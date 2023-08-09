08babe0e3157ea1d928cbe64f2e62d84f3912372
haoting <haoting3462@gmail.com>
2023-08-09 15:46:17 +0800

    feat: experience card

--- app/sections/ExperienceCard.tsx
+++ app/sections/ExperienceCard.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image"
-
 interface ExperienceCardProps {
   jobTitle: string
   employmentType: string
@@ -10,23 +8,43 @@ interface ExperienceCardProps {
   companyLink?: string
 }
 
-const ExperienceCard = ({jobTitle, employmentType, duration, company, desc, image, companyLink}: ExperienceCardProps) => {
+const ExperienceCard = ({
+  jobTitle,
+  employmentType,
+  duration,
+  company,
+  desc,
+  image,
+  companyLink
+}: ExperienceCardProps) => {
   return (
-    <div className="p-4 shadow  rounded bg-opacity-60 grid dark:shadow-slate hover:shadow-md transition-all">
-<h3>{jobTitle}</h3>
-<div>{employmentType}</div>
-<div>{duration}</div>
-<div>{company}</div>
-<ul>
-  {desc.map((li, i) => (
-    <li key={i}>{li}</li>
-  )) }
-</ul>
-<div>
-  <img src={image} height={28} width={28} alt={`${company} image`} />
+    <div
+      className={`border border-gray-300 rounded-lg shadow-md bg-bunker-50 dark:bg-gray-800 dark:border-gray-700 relative overflow-hidden `}
+    >
+      <img
+        src={image}
+        className='absolute opacity-20 -bottom-16 -right-8 overflow-hidden'
+        width={400}
+      ></img>
+      <div className='backdrop-blur w-full h-full p-4 rounded-lg flex flex-col gap-1'>
+        <div className='flex flex-col sm:flex-row justify-between items-center w-full'>
+          <h3 className='text-lg font-semibold flex-shrink-0'>{jobTitle}</h3>
+          <span className='text-gray-500 dark:text-bunker-400 text-xs'>
+            {employmentType} | {duration}
+          </span>
+        </div>
+        <div className='text-gray-700 dark:text-bunker-100 self-center sm:self-start'>
+          {company}
+        </div>
+        <ul className='mt-2 space-y-1'>
+          {desc.map((li, i) => (
+            <li key={i} className='text-gray-700 dark:text-bunker-200 text-xs'>
+              {li}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    </div>
-
   )
 }
 

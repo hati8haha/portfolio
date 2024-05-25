4cd9c4e2d6fb421bce7896d14787e36270f90a45
HaoTing Cheng <haoting3462@gmail.com>
2024-05-25 15:39:42 +0000

    style: Fix formatting issues  by using consistent quotes and semicolons

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-import OnScrollInViewAnimation from '@/components/motion/OnScrollInViewAnimation'
-import Hero from '@/components/sections/Hero'
-import Experience from '../components/sections/Experience'
-import ExperienceCard from '../components/sections/ExperienceCard'
-import PortfolioSection from '@/components/sections/PortfolioSection'
-import SkillSection from '@/components/sections/SkillsSection'
-import CommunitySection from '@/components/sections/CommunitySection'
-import Head from 'next/head'
+import OnScrollInViewAnimation from "@/components/motion/OnScrollInViewAnimation";
+import Hero from "@/components/sections/Hero";
+import Experience from "../components/sections/Experience";
+import ExperienceCard from "../components/sections/ExperienceCard";
+import PortfolioSection from "@/components/sections/PortfolioSection";
+import SkillSection from "@/components/sections/SkillsSection";
+import CommunitySection from "@/components/sections/CommunitySection";
+import Head from "next/head";
+import { jobsData } from "@/components/sections/data";
 
-import { jobsData } from '@/components/sections/data'
 export default function Home() {
   const meta = {
-    title: 'Frontend Developer Portfolio',
+    title: "Frontend Developer Portfolio",
     description: "Welcome to Haoting Cheng's personal portfolio website!",
-    image: '/head-img.png',
-    url: `https://haoting-cheng.vercel.app/`
-  }
+    image: "/head-img.png",
+    url: `https://haoting-cheng.vercel.app/`,
+  };
 
   return (
-    <main className='flex flex-col dark:bg-opacity-10 items-center justify-between p-8 xl:p-24 dark:bg-bunker-950 bg-bunker-100 bg-opacity-20 backdrop-blur-lg relative'>
+    <main className="flex flex-col dark:bg-opacity-10 items-center justify-between p-8 xl:p-24 dark:bg-bunker-950 bg-bunker-100 bg-opacity-20 backdrop-blur-lg relative">
       <Head>
         <title>{meta.title}</title>
-        <meta name='description' content={meta.description} />
-        <meta property='og:title' content={meta.title} />
-        <meta property='og:description' content={meta.description} />
-        <meta property='og:image' content={meta.image} />
-        <meta property='og:url' content={meta.url} />
-        <meta name='twitter:title' content={meta.title} />
-        <meta name='twitter:description' content={meta.description} />
-        <meta name='twitter:image' content={meta.image} />
-        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="og:url" content={meta.url} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Hero />
       <PortfolioSection />
@@ -46,5 +46,5 @@ export default function Home() {
       </Experience>
       <SkillSection />
     </main>
-  )
+  );
 }
